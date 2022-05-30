81721125be91693c05376192e35af8292a8cf4ec
sally <sally@geekstudio.co.kr>
2022-05-30 23:06:00 +0900

    DOCS : "props"를 통해 데이터 전달하기

--- src/components/expenseItem.js
+++ src/components/expenseItem.js
@@ -1,17 +1,14 @@
 import './ExpenseItem.css';
 
-function ExepenseItem() {
+function ExepenseItem(props) {
     /* regular Javascript (React와 상관없음) */
-    const expenseDate = new Date(2022, 4, 30);
-    const expenseTitle = 'Car Insurance';
-    const exepenseAmount = 294.67;
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toString()}</div>
+            <div>{props.date.toString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${exepenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
 
             </div>
         </div>
